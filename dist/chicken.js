632cb90258088c9bb8596d35c9ccf2476b1462f8
aurelianonava <sersinfin@gmail.com>
2024-04-26 12:31:40 -0700

    new commit

---
+++ dist/chicken.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Chicken = void 0;
+class Chicken {
+    cluck() {
+        console.log('Cluck Gallina and Chicken and Turkey and Guajolote!');
+    }
+}
+exports.Chicken = Chicken;
+//# sourceMappingURL=chicken.js.map
